6faf0b14e06c9990224581851ab7f04f871f7a9e
Taras <topanasenko85@gmail.com>
2023-02-16 19:14:11 +0200

    task-2

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,13 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const listIngredients = document.querySelector('#ingredients');
+console.log(listIngredients);
+
+const elementList = ingredients.forEach(function (value) {
+  const list = document.createElement('li');
+  list.classList.add('item');
+  list.textContent = value;
+  listIngredients.appendChild(list);
+});
