8c1c22d0753c74bc39a981f52561c0cdeafc7f70
Taras <topanasenko85@gmail.com>
2023-02-18 21:27:44 +0200

    task-6

---
+++ js/task-06.js
@@ -0,0 +1,21 @@
+const inputText = document.querySelector('input');
+// console.log(inputText);
+
+let lengthChar = 0;
+
+inputText.addEventListener('focus', () => {
+  inputText.addEventListener('input', event => {
+    lengthChar = event.currentTarget.value.length;
+  });
+});
+
+inputText.addEventListener('blur', () => {
+  //   console.log(lengthChar);
+  //   console.log(inputText.dataset.length);
+
+  if (inputText.dataset.length > lengthChar) {
+    inputText.className = 'invalid';
+  } else {
+    inputText.className = 'valid';
+  }
+});
