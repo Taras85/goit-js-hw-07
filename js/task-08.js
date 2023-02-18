db6fde121aff4a9c5d6d3caddaf0682cd2e2fa82
Taras <topanasenko85@gmail.com>
2023-02-18 23:40:18 +0200

    task-8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,23 @@
+const loginForm = document.querySelector('.login-form');
 
+loginForm.addEventListener('submit', handleSubmit);
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const {
+    elements: { email, password },
+  } = event.currentTarget;
+
+  if (email.value === '' || password.value === '') {
+    return alert(
+      'All fields are due buti orders! \n Всі поля повині бути заповнені',
+    );
+  }
+
+  const user = {};
+  user.email = email.value;
+  user.password = password.value;
+
+  console.log(user);
+  event.currentTarget.reset();
+}
