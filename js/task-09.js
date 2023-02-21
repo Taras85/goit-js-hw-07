db9989485572d2630e54a1b7985bcc03b253df1d
Taras <topanasenko85@gmail.com>
2023-02-21 23:06:44 +0200

    task-9 bag

--- js/task-09.js
+++ js/task-09.js
@@ -1,15 +1,13 @@
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215)
-    .toString(16)
-    .padStart(6, 0)}`;
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
 const butonChangeColor = document.querySelector('.change-color');
 const bodyChangeColor = document.querySelector('body');
 const changedColor = document.querySelector('.color');
 
 butonChangeColor.addEventListener('click', () => {
-  bodyChangeColor.style.backgroundColor = getRandomHexColor();
-  changedColor.textContent = `-${bodyChangeColor.style.backgroundColor}`;
+  changedColor.textContent = getRandomHexColor();
+  bodyChangeColor.style.backgroundColor = changedColor.textContent;
 
-  console.log(bodyChangeColor.style.backgroundColor);
+  console.log(bodyChangeColor.style.background);
 });
