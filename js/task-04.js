acfac43435c412a23173a1550e5d0333f3ca8884
Taras <102531278+Taras85@users.noreply.github.com>
2023-02-22 16:42:42 +0200

    12

--- js/task-04.js
+++ js/task-04.js
@@ -1,13 +1,13 @@
 let counterValue = 0;
 const butonDecrement = document.querySelector('[data-action="decrement"]');
 const butonIncrement = document.querySelector('[data-action="increment"]');
-const valueResult = document.querySelector('#value');
+const valueResult = document.querySelector("#value");
 
-butonDecrement.addEventListener('click', () => {
+butonDecrement.addEventListener("click", () => {
   counterValue -= 1;
   valueResult.textContent = `${counterValue}`;
 });
-butonIncrement.addEventListener('click', () => {
+butonIncrement.addEventListener("click", () => {
   counterValue += 1;
   valueResult.textContent = `${counterValue}`;
 });
