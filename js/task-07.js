91444aa08cedc43f273bd330bf0813bf0d4d068b
Taras <topanasenko85@gmail.com>
2023-02-25 17:24:53 +0200

    start

--- js/task-07.js
+++
@@ -1,8 +0,0 @@
-const fontSizeControl = document.querySelector('#font-size-control');
-const fontSizeText = document.querySelector('#text');
-
-fontSizeControl.addEventListener('input', () => {
-  console.log(fontSizeControl.value);
-
-  fontSizeText.style.fontSize = `${fontSizeControl.value}px`;
-});
