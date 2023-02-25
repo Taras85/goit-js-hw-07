8cb7dc4cdbd930e30013094fd255ca7726d3b5af
Taras <topanasenko85@gmail.com>
2023-02-26 00:37:16 +0200

    gallery-1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,8 +1,9 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
-console.log(galleryItems);
+// console.log(galleryItems);
 
 const galleryList = document.querySelector('.gallery');
+
 galleryList.addEventListener('click', clickImage);
 
 const generateHtml = (description, original, preview) => {
@@ -30,18 +31,34 @@ const generateGallery = galleryItems
 
 galleryList.insertAdjacentHTML('afterbegin', generateGallery);
 
-function clickImage(event) {}
+// ***//***// */
 
-//
-// console.log(generateHtml);
+const instance = basicLightbox.create(
+  `
+<img  src="">`,
+  {
+    onShow: instance => {
+      window.addEventListener('keydown', onEscKeyPress);
+    },
+    onClose: instance => {
+      window.removeEventListener('keydown', onEscKeyPress);
+    },
+  },
+);
 
-// <div class="gallery__item">
-//   <a class="gallery__link" href="large-image.jpg">
-//     <img
-//       class="gallery__image"
-//       src="small-image.jpg"
-//       data-source="large-image.jpg"
-//       alt="Image description"
-//     />
-//   </a>
-// </div>;
+function clickImage(event) {
+  event.preventDefault();
+  const datasetSource = event.target.dataset.source;
+  const imgAlt = event.target.alt;
+  if (!datasetSource) return;
+  instance.element().querySelector('img').src = datasetSource;
+  instance.element().querySelector('img').alt = imgAlt;
+  instance.show();
+}
+
+function onEscKeyPress(event) {
+  if (event.code !== 'Escape') return;
+  instance.close();
+}
+
+// width="800" height="auto"
